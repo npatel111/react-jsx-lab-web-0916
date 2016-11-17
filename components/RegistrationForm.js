568b8f356ab85400674587d616e4d2d20a47a577
Niti Nrw <niti.patel@flatironschool.com>
2016-11-16 19:27:18 -0500

    Done.

---
+++ components/RegistrationForm.js
@@ -0,0 +1,15 @@
+const React = require('react');
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type= "text" />
+        <input type= "password" />
+        <button type="submit"> </button>
+      </form>
+    );
+  }
+}
+
+module.exports = RegistrationForm;
